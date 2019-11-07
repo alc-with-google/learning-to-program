c390f4d1e355edcecc34dda33edbf01ef1872fdc
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-11-07 21:09:43 +0100

    More Express Stuff

--- gradeBook/program.js
+++ gradeBook/program.js
@@ -8,5 +8,16 @@ app.get('/', function(req, res){
     res.send('Hello World');
 });
 
+app.get('/grade', function(req, res){
+    var grades = req.query.grades.split(',');
+    for (var i=0; i< grades.length; i++){
+        book.addGrade(parseInt(grades[i]));
+    }
+    var average = book.getAverage();
+    var letter = book.getLetterGrade();
+
+    res.send('Your average is ' + average + '; Grade is ' + letter)
+});
+
 app.listen(3000)
 console.log("Server is ready...")
